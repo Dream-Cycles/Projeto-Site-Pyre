a3a9983fd6c7f271207949ffaa36af9a5edef556
Dream-Cycles <Seabest2@proton.me>
2026-06-19 16:21:12 -0300

    Feat: Correções gerais e estilização

--- custom/javascript/var.js
+++ custom/javascript/var.js
@@ -1,7 +1,7 @@
 // dom.js
 
 export const previewCard = document.querySelectorAll(".card-preview");
-export const previewArea = document.querySelector(".preview");
+export const previewArea = document.querySelector(".preview-section");
 export const overlay = document.querySelector(".overlay");
 export const cdContainer = document.querySelectorAll(".cd-container");
 export const videoPreview = document.querySelector(".preview-video")
